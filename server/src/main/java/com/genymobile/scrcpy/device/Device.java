779d5e32a0b50b08a701e62751f9b3d247316d56
Romain Vimont <rom@rom1v.com>
2021-06-13 22:32:36 +0200

    Add a method to send a broadcast
    Implement the equivalent of `Context.sendBroadcast(Intent)`.
    
    PR #6852 <https://github.com/Genymobile/scrcpy/pull/6852>

--- server/src/main/java/com/genymobile/scrcpy/device/Device.java
+++ server/src/main/java/com/genymobile/scrcpy/device/Device.java
@@ -312,4 +312,8 @@ public final class Device {
         }
         am.startActivity(launchIntent, options);
     }
+
+    public static void sendBroadcast(Intent intent) {
+        ServiceManager.getActivityManager().sendBroadcast(intent);
+    }
 }
